bd7e34dfe420e6759b863233445b38a7115e8da3
chris <christofferraahauge@gmail.com>
2022-09-15 10:21:59 +0200

    Jeg har lavet selection sort

--- Selectionsort.py
+++ Selectionsort.py
@@ -1,13 +1,13 @@
 def selectionSort(items):
-    for i in range(len(items) - 1):
+    for i in range(len(items) - 1):#holder styr på hvilket index vi er i gang med
         smallest = i
         print(items)
-        for j in range(i + 1, len(items)):
-            if items[smallest] > items[j]:
+        for j in range(i + 1, len(items)):#sortere og vælger næste index
+            if items[smallest] > items[j]:#tjekker om i er større end det næste tal
                 smallest = j
-            items[smallest], items[i] = items[i], items[smallest]
+            items[smallest], items[i] = items[i], items[smallest]#bytter rundt på tallene
 
-    return items
+    return items#kører programmet igennem igen
 
 
 items = [7, 4, 9, 2, 3]
